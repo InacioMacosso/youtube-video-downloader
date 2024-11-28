331ecc3c7b3d4553f8e646648315b18f473d0b57
Moukengue <118891505+Moukengue@users.noreply.github.com>
2024-11-28 21:12:45 +0100

    Commit initial

---
+++ src/test/java/com/macosso/youtube_video_downloader/YoutubeVideoDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macosso.youtube_video_downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeVideoDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
